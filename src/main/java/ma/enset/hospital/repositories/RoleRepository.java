315c9c0f98aab3c97f308b8f8bd95d3572d9f99d
DOUHIChaimae <c.douhi@etu.enset-media.ac.ma>
2023-03-17 12:00:01 +0100

    adding user and role repositories

---
+++ src/main/java/ma/enset/hospital/repositories/RoleRepository.java
@@ -0,0 +1,7 @@
+package ma.enset.hospital.repositories;
+
+import ma.enset.hospital.entities.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role,Long> {
+}

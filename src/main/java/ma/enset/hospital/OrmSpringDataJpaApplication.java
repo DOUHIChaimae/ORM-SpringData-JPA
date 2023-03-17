343481e90ab1bc2c436357703201410dd616bc43
DOUHIChaimae <c.douhi@etu.enset-media.ac.ma>
2023-03-17 09:31:02 +0100

    adding @JsonProperty annotation and implementing a PatientRestController

--- src/main/java/ma/enset/hospital/OrmSpringDataJpaApplication.java
+++ src/main/java/ma/enset/hospital/OrmSpringDataJpaApplication.java
@@ -5,6 +5,7 @@ import ma.enset.hospital.repositories.ConsultationRepository;
 import ma.enset.hospital.repositories.MedecinRepository;
 import ma.enset.hospital.repositories.PatientRepository;
 import ma.enset.hospital.repositories.RendezVousRepository;
+import ma.enset.hospital.services.IHospitalService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -22,47 +23,41 @@ public class OrmSpringDataJpaApplication {
     }
 
     @Bean
-    CommandLineRunner start(PatientRepository patientRepository,
-                            MedecinRepository medecinRepository,
-                            RendezVousRepository rendezVousRepository,
-                            ConsultationRepository consultationRepository) {
+    CommandLineRunner start(IHospitalService hospitalService, PatientRepository patientRepository, RendezVousRepository rendezVousRepository, MedecinRepository medecinRepository) {
         return args -> {
-            Stream.of("douhi1", "douhi2", "douhi3")
+            Stream.of("patient1", "patient2", "patient3")
                     .forEach(name -> {
-                                Patient patient = new Patient();
-                                patient.setName(name);
-                                patient.setBirthday(new Date());
-                                patient.setScore((int) Math.random() * 100);
-                                patient.setIsSick(false);
-                                patientRepository.save(patient);
-                            }
-                    );
-            Stream.of("medecin1", "medecin2", "medecin3", "medecin5")
+                        Patient patient = new Patient();
+                        patient.setName(name);
+                        patient.setBirthday(new Date());
+                        patient.setIsSick(false);
+                        hospitalService.savePatient(patient);
+                    });
+            Stream.of("medecin1", "medecin2", "medecin3", "medecin4")
                     .forEach(name -> {
                                 Medecin medecin = new Medecin();
                                 medecin.setName(name);
                                 medecin.setSpeciality(Math.random() > 0.5 ? "Cardio" : "dentist");
                                 medecin.setEmail(name + "@gmail.com");
-                                medecinRepository.save(medecin);
+                                hospitalService.saveMedecin(medecin);
                             }
                     );
-            Patient patient = patientRepository.findById(1L).orElse(null);
-            Patient patient1 = patientRepository.findByName("patient3");
-            Medecin medecin = medecinRepository.findByName("medecin5");
+            Patient patient = patientRepository.findAll().get(0);
+            Patient patient1 = patientRepository.findByName("patient1");
+            Medecin medecin = medecinRepository.findByName("medecin2");
             RendezVous rendezVous = new RendezVous();
             rendezVous.setDate(new Date());
             rendezVous.setStatus(RDVStatus.PENDING);
             rendezVous.setMedecin(medecin);
             rendezVous.setPatient(patient);
-            rendezVousRepository.save(rendezVous);
+            hospitalService.saveRDV(rendezVous);
             Consultation consultation = new Consultation();
             consultation.setConsultationDate(new Date());
             consultation.setRendezVous(rendezVous);
             consultation.setRapport("Rapport de la consultation..");
-            consultationRepository.save(consultation);
+            hospitalService.saveConsultation(consultation);
         };
     }
-
 
 }
 

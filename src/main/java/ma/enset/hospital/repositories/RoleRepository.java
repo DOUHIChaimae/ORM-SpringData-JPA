ce389f2d63661f00dac4b71708447e5163168476
DOUHIChaimae <c.douhi@etu.enset-media.ac.ma>
2023-03-17 15:15:02 +0100

    Implementing the UserService interface

--- src/main/java/ma/enset/hospital/repositories/RoleRepository.java
+++ src/main/java/ma/enset/hospital/repositories/RoleRepository.java
@@ -3,5 +3,7 @@ package ma.enset.hospital.repositories;
 import ma.enset.hospital.entities.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RoleRepository extends JpaRepository<Role,Long> {
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Role findByRoleName(String roleName);
+
 }
---
+++ src/main/java/ma/enset/hospital/services/UserServiceImpl.java
@@ -0,0 +1,50 @@
+package ma.enset.hospital.services;
+
+import jakarta.transaction.Transactional;
+import lombok.AllArgsConstructor;
+import ma.enset.hospital.entities.Role;
+import ma.enset.hospital.entities.User;
+import ma.enset.hospital.repositories.RoleRepository;
+import ma.enset.hospital.repositories.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@Transactional
+@AllArgsConstructor
+public class UserServiceImpl implements UserService {
+    private UserRepository userRepository;
+    private RoleRepository roleRepository;
+
+    @Override
+    public User addNewUser(User user) {
+        user.setUserId(UUID.randomUUID().toString());
+        return userRepository.save(user);
+    }
+
+    @Override
+    public Role addNewRole(Role role) {
+        return roleRepository.save(role);
+    }
+
+    @Override
+    public User findUserByUserName(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    @Override
+    public Role findRoleByRoleName(String roleName) {
+        return roleRepository.findByRoleName(roleName);
+    }
+
+    @Override
+    public void addRoleToUser(String username, String roleName) {
+        User user = findUserByUserName(username);
+        Role role = findRoleByRoleName(roleName);
+        if (user.getRoles() != null) {
+            user.getRoles().add(role);
+            role.getUsers().add(user);
+        }
+    }
+}

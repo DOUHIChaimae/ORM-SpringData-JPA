913ea024d53fa142db339c169adf84929a590394
DOUHIChaimae <c.douhi@etu.enset-media.ac.ma>
2023-03-17 16:00:53 +0100

    adding some users and some roles to the DB

--- src/main/java/ma/enset/hospital/OrmSpringDataJpaApplication.java
+++ src/main/java/ma/enset/hospital/OrmSpringDataJpaApplication.java
@@ -6,13 +6,13 @@ import ma.enset.hospital.repositories.MedecinRepository;
 import ma.enset.hospital.repositories.PatientRepository;
 import ma.enset.hospital.repositories.RendezVousRepository;
 import ma.enset.hospital.services.IHospitalService;
+import ma.enset.hospital.services.UserService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 import java.util.Date;
-import java.util.List;
 import java.util.stream.Stream;
 
 @SpringBootApplication
@@ -23,7 +23,7 @@ public class OrmSpringDataJpaApplication {
     }
 
     @Bean
-    CommandLineRunner start(IHospitalService hospitalService, PatientRepository patientRepository, RendezVousRepository rendezVousRepository, MedecinRepository medecinRepository) {
+    CommandLineRunner start(IHospitalService hospitalService, PatientRepository patientRepository, RendezVousRepository rendezVousRepository, MedecinRepository medecinRepository, UserService userService) {
         return args -> {
             Stream.of("patient1", "patient2", "patient3")
                     .forEach(name -> {
@@ -56,6 +56,30 @@ public class OrmSpringDataJpaApplication {
             consultation.setRendezVous(rendezVous);
             consultation.setRapport("Rapport de la consultation..");
             hospitalService.saveConsultation(consultation);
+
+            User user1 = new User();
+            user1.setUsername("user1");
+            user1.setPassword("user123");
+            userService.addNewUser(user1);
+
+            User user2 = new User();
+            user2.setUsername("user2");
+            user2.setPassword("admin123");
+            userService.addNewUser(user2);
+
+            Stream.of("STUDENT", "USER", "ADMIN")
+                    .forEach(role -> {
+                        Role role1 = new Role();
+                        role1.setRoleName(role);
+                        userService.addNewRole(role1);
+
+                    });
+            userService.addRoleToUser("user1", "STUDENT");
+            userService.addRoleToUser("user1", "USER");
+            userService.addRoleToUser("admin", "USER");
+            userService.addRoleToUser("admin", "ADMIN");
+
+
         };
     }
 
